444fcd1e44bb8a8196ad67e22029a216b77c6f4b
Will Strimling <w@ml.to>
2017-04-24 23:05:23 -0700

    begin position algo

--- src/layout.js
+++ src/layout.js
@@ -36,105 +36,183 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     .data(data)
     .label((datum) => datum.label)
     .mark((datum) => datum.mark)
-    .baseline((datum) => datum.baseline)
     .padding(5)
-    .rotation(20)
+    .pivot(20)
     .line({length: 10, angle: 45})
     .extent([100, 100])
     .layout()
 */
 
-var ANGLE = [0, 45, 90, 135, 180, 225, 270, 315, 360];
+var ANGLE = 45;
+var PADDING = 10;
+
+// methods for placement
+function TOP(d) {
+  // calculate marks dimensions (it should be a rect)
+  // if d.label width + d.padding * 2 < d.mark's width
+    // if d.label height + d.padding * 2 < d.mark's height
+      // place label at top, inside of mark (w.r.t. padding)
+    // else place d.label above mark w.r.t. padding
+  // else attempt to place label above mark @ padding
+    // while occluding another label or mark, and not higher than line.length, move up
+    // add label, add line connecting label
+}
+
+function RIGHT(d) {
+  // calculate marks dimensions (it should be a rect)
+  // if d.label height + d.padding * 2 < d.mark's height
+    // if d.label width + d.padding * 2 < d.mark's width
+      // place label at right, inside of mark (w.r.t. padding)
+    // else place d.label right of mark w.r.t. padding
+  // else attempt to place label right of mark @ padding
+    // while occluding another label or mark, and not higher than line.length, move right
+    // add label, add line connecting label
+}
+
+function INSIDE(d) {
+  // find inscribed rectangles s.t. label.area + padding^2 + padding * label.width + padding * label.height <= rect.area, track pivot
+  // if best rectangle with rect.angle < pivot
+    // place label @ center of rectangle with sample angle
+  // else
+    // put label above center or largest recentangle,
+    // while distance from center < line.length
+      // try placing label at distance, draw line
+      // move up if not
+    // try 45 degree alternatives from center
+}
+
+function ARC(d) {
+
+}
+
+function LINE(d) {
+
+}
+
+function SCATTER(d) {
+
+}
+
+function position(label) {
+  // if mark is a rect
+    // determine orientation through l.collections.marks
+    // if can't determine orientation use CENTER
+    // if orientation = vertical, use RIGHT
+    // if orientation = horizontal, use TOP
+  // if mark is a polygon/shape (area, geo)
+    // use INSIDE
+  // if mark is an arc
+    // use ARC (fallback to area)
+  // if mark is a line
+    // use LINE
+  // if mark is a point
+    // use SCATTER
+}
 
 export default function() {
   var layout = {},
-      data, label, mark, baseline, padding, extent, rotation, line;
+      data = [],
+      line = null,
+      orient = null,
+      padding = PADDING,
+      label, mark, extent, pivot, line;
 
   layout.layout = function () {
+    var _marks = [];
+    var _labels = [];
 
+    var Labels = data.map(function (d) {
+      var m = mark(d);
+      var l = label(d);
+      l = l[+l[0], +l[1]]
+      _marks.push(m);
+      _labels.push(m);
+      return {
+        collections: {
+          marks: _marks,
+          labels: _labels
+        },
+        config: {
+          padding: padding,
+          pivot: pivot,
+          line: line
+        },
+        mark: m,
+        line: null, // [[x1, y1], [x2, y2]] of line connecting label to mark
+        opacity: 0.0, // is label visible
+        x: 0, // x location of label
+        y: 0, // y location of label
+        width: +l[0],
+        height: +l[1],
+        angle: 0 // angle to display label at
+      }
+    }).sort(function smallest(a, b) {
+      return a.width - b.width;
+    });
+
+    return Labels.map(position);
   };
 
-  layout.data = function (_data) {
-    if (!arguments.length) {
-      return data;
-    }
+  layout.data = function (d) {
+    if (!arguments.length) return data;
 
-    data = _data;
+    data = d;
     return layout;
   };
 
-  layout.label = function (_label) {
-    if (!arguments.length) {
-      return label;
-    }
+  layout.label = function (d) {
+    if (!arguments.length) return label;
 
-    label = callable(_label);
+    label = callable(d);
     return layout;
   };
 
-  layout.mark = function (_mark) {
-    if (!arguments.length) {
-      return mark;
-    }
+  layout.mark = function (d) {
+    if (!arguments.length) return mark;
 
-    mark = callable(_mark);
+    mark = callable(d);
     return layout;
   };
 
-  layout.baseline = function (_baseline) {
-    if (!arguments.length) {
-      return baseline;
-    }
+  layout.padding = function (d) {
+    if (!arguments.length) return padding;
 
-    baseline = callable(_baseline);
+    padding = d;
     return layout;
   };
 
-  layout.padding = function (_padding) {
-    if (!arguments.length) {
-      return padding;
-    }
+  layout.extent = function (d) {
+    if (!arguments.length) return extent;
 
-    padding = _padding;
+    extent = [+d[0], +d[1]];
     return layout;
   };
 
-  layout.extent = function (_extent) {
-    if (!arguments.length) {
-      return extent;
-    }
+  layout.pivot = function (d) {
+    if (!arguments.length) return pivot;
 
-    extent = [+_extent[0], +_extent[1]];
+    pivot = d;
     return layout;
   };
 
-  layout.rotation = function (_rotation) {
-    if (!arguments.length) {
-      return rotation;
-    }
+  layout.line = function (d) {
+    if (!arguments.length) return line;
 
-    rotation = _rotation;
-    return layout;
-  };
-
-  layout.line = function (_line) {
-    if (!arguments.length) {
-      return line;
-    }
-
-    if (typeof _line !== "object") {
+    if (typeof d !== "object") {
       line = {
-        length: _line,
+        length: d,
         angle: ANGLE
       };
-    } else if (!_line.hasOwnProperty(angle) || _line.angle == null) {
-      line = _line;
+    } else if (!d.hasOwnProperty(angle) || d.angle == null) {
+      line = d;
       line.angle = ANGLE;
-    } else if (!Array.isArray(_line.angle)) {
-      line = _line;
+    } else {
+      line = d;
+    }
+
+    if (!Array.isArray(line.angle)) {
       var iterator = line.angle;
       line.angle = [];
-
       for (var i = 0; i < 360; i += iterator) {
         line.angle.push(i);
       }
@@ -147,11 +225,10 @@ export default function() {
 };
 
 // convert arg into callable function
-function callable(_callable) {
-  if (typeof _callable !== "function") {
-    return function () {
-      return _callable;
-    };
+function callable(d) {
+  if (typeof d === "function") return d;
+
+  return function () {
+    return d;
   }
-  return _callable;
 }
